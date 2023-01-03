7bb4760147a92318f6aff74456ad344d68a5d053
ayemereodia2 <odiadavid2@gmail.com>
2023-01-04 00:37:37 +0100

    swift package added

--- CustomFrameworkProject/SDK.swift
+++ CustomFrameworkProject/SDK.swift
@@ -6,8 +6,8 @@
 //
 
 import Foundation
-struct SDK {
-    static func doSomeWork() {
-        print("Doing some work...")
-    }
-}
+//struct SDK {
+//    static func doSomeWork() {
+//        print("Doing some work...")
+//    }
+//}
