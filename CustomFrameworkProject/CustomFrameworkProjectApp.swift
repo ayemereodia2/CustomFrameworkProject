661ba3e2f9dcce93c59565fc43933cf78c1ba13e
ayemereodia2 <odiadavid2@gmail.com>
2023-01-04 00:20:26 +0100

    Initial Commit

---
+++ CustomFrameworkProject/CustomFrameworkProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomFrameworkProjectApp.swift
+//  CustomFrameworkProject
+//
+//  Created by ANDELA on 04/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomFrameworkProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
